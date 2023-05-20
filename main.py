aa05c20dbac05f319462befb80d0181e897f81a0
klop2211 <58390003+klop2211@users.noreply.github.com>
2023-05-20 19:23:07 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+class MainGUI():
+    def setUI(self):
+        searchEntry = Entry()
+
+
+    def __init__(self):
+        self.window = Tk()
+        self.setUI()
+
+
+        self.window.mainloop()
+
+
+MainGUI()
