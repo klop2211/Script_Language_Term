a94d40af96a3b4f9d437ecf5272b9de8a246fdc0
akdl6632 <akdl6632@naver.com>
2023-05-30 01:54:20 +0900

    검색, 클릭 이벤트 구현
    엑셀 파일 첫번째가 이상한 값 있어서 첫번째 빼고 모든 값을 읽어왔음
    '서울특별시' , '종로구', '사직동' 이런 형태로 데이터가 저장되어있어서 한 문장으로 만들고 합치게 했음
    검색은 그냥 무식하게 처음부터 다 읽어와서 비교하도록 클릭도 검색이랑 똑같이했어

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
 from tkinter import *
+import requests
+import xml.etree.ElementTree as ET
+import time
+from urllib.request import Request, urlopen
+from urllib import parse
+from urllib.parse import urlencode, quote_plus
+
+# 소나무, 참나무만 4~6 데이터를 제공
+# 소나무
+url = 'http://apis.data.go.kr/1360000/HealthWthrIdxServiceV3/getPinePollenRiskIdxV3'
+service_key = "oWT9dq/7S6E11bbGOg2qY18HrHxLHQov6dy6WglV6AzFYqnOUC0YnQt/3GblAB1ygHHoWQpDuY0Wqc8grUD5oQ=="
+now = time
+now = now.strftime('%Y%m%d06') #2023052605 같은 형태로 전달해 줘야함
+queryParams = {'serviceKey': service_key, 'pageNo': '1', 'numOfRows': '10', 'dataType': 'XML', 'areaNo': '', 'time': now}
+
+response = requests.get(url, params=queryParams)
+print(response.text)
+root = ET.fromstring(response.text)
+
+# 읽어올  값
+for item in root.iter("item"):
+    code = item.findtext("code") # 지수코드
+    areaNo = item.findtext("areaNo") # 지점코드
+    date = item.findtext("date") # 발표시간
+    today = item.findtext("today") # 오늘 예측값
+    tomorrow = item.findtext("tomorrow") # 내일 예측값
+    dayaftertomorrow = item.findtext("dayaftertomorrow") # 모레 예측값
+    todaysaftertomorrow = item.findtext("todaysaftertomorrow") # 글피 예측값
+
+    data = [code, areaNo, date, today, tomorrow, dayaftertomorrow, todaysaftertomorrow]
 
 class MainGUI():
     def setUI(self):
@@ -47,7 +77,7 @@ class MainGUI():
 
     def __init__(self):
         self.window = Tk()
-        self.window.title("에취")
+        self.window.title("플라워타임")
         self.window.geometry("600x800")
         self.setUI()
 
